fc8fe4a5ed53fd9c9bd28bdc4bf6c54e15c0d149
lgick <lgick@yandex.ru>
2014-12-21 19:06:50 +0300

    перенос библиотек в /lib и оптимизация их работы на сервере и клиенте

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.set('view engine', 'jade');
 
 app.use(express.favicon(path.join(__dirname, '/public/img/favicon.ico')));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/lib')));
 app.use(express.static(path.join(__dirname, '/game')));
 
 require('./routes')(app);
