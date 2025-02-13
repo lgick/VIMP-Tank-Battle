ffa58307b5ab17ccced2ab67426d700f820eb950
lgick <lgick@yandex.ru>
2025-02-13 11:23:27 +0300

    modules

---
+++ backend/game/modules/stat.js
@@ -0,0 +1,247 @@
+// Singleton Stat
+let stat;
+
+class Stat {
+  constructor(config, teams) {
+    if (stat) {
+      return stat;
+    }
+
+    stat = this;
+
+    this._config = config;
+
+    this._head = {};
+    this._body = {};
+
+    this._lastHead = [];
+    this._lastBody = [];
+
+    for (const p in teams) {
+      if (teams.hasOwnProperty(p)) {
+        this._head[teams[p]] = [teams[p], []];
+        this._body[teams[p]] = {};
+      }
+    }
+  }
+
+  // сбрасывает статистику
+  reset() {
+    for (const teamID in this._body) {
+      if (this._body.hasOwnProperty(teamID)) {
+        const bodyStats = this._body[teamID];
+
+        for (const gameID in bodyStats) {
+          if (bodyStats.hasOwnProperty(gameID)) {
+            const stat = bodyStats[gameID];
+            stat[2] = this.getDefaultBody(stat[2]);
+            this._lastBody.push(stat);
+          }
+        }
+      }
+    }
+
+    for (const teamID in this._head) {
+      if (this._head.hasOwnProperty(teamID)) {
+        const stat = this._head[teamID];
+
+        for (const p in this._config) {
+          if (this._config.hasOwnProperty(p)) {
+            const conf = this._config[p];
+
+            if (conf.headSync) {
+              this.updateHeadSync(teamID, p);
+            } else if (typeof conf.headValue !== 'undefined') {
+              stat[1][conf.key] = conf.headValue;
+            }
+          }
+        }
+
+        this._lastHead.push(stat);
+      }
+    }
+  }
+
+  // возвращает дефолтные данные для body
+  getDefaultBody(stat = []) {
+    for (const p in this._config) {
+      if (this._config.hasOwnProperty(p)) {
+        const conf = this._config[p];
+
+        if (typeof conf.bodyValue !== 'undefined') {
+          stat[conf.key] = conf.bodyValue;
+        }
+      }
+    }
+
+    return stat;
+  }
+
+  // добавляет пользователя
+  addUser(gameID, teamID, data) {
+    this._body[teamID][gameID] = [gameID, teamID, this.getDefaultBody()];
+
+    if (typeof data === 'object') {
+      this.updateUser(gameID, teamID, data);
+    }
+  }
+
+  // удаляет пользователя и возвращает его данные
+  removeUser(gameID, teamID) {
+    let data = this._body[teamID][gameID];
+    const stat = {};
+
+    delete this._body[teamID][gameID];
+    this._lastBody.push([data[0], data[1], null]);
+
+    data = data[2];
+
+    // преобразование данных пользователя из массива в объект и
+    // обновление head
+    for (const p in this._config) {
+      if (this._config.hasOwnProperty(p)) {
+        const conf = this._config[p];
+        const value = data[conf.key];
+
+        stat[p] = value;
+
+        if (conf.headSync === true) {
+          this.updateHeadSync(teamID, p, true);
+        }
+      }
+    }
+
+    return stat;
+  }
+
+  // перемещает пользователя в новую команду
+  moveUser(gameID, teamID, newTeamID) {
+    this.addUser(gameID, newTeamID, this.removeUser(gameID, teamID));
+  }
+
+  // обновляет статистику пользователя
+  updateUser(gameID, teamID, data) {
+    const stat = this._body[teamID][gameID];
+
+    for (const p in data) {
+      if (data.hasOwnProperty(p)) {
+        const conf = this._config[p];
+        const method = conf.bodyMethod;
+        const value = data[p];
+
+        // если метод 'замена'
+        if (method === '=') {
+          stat[2][conf.key] = value;
+
+          // иначе если метод 'добавление'
+        } else if (method === '+') {
+          stat[2][conf.key] += value;
+        }
+
+        if (conf.headSync === true) {
+          this.updateHeadSync(teamID, p, true);
+        }
+      }
+    }
+
+    this._lastBody.push(stat);
+  }
+
+  // обновляет статистику head
+  updateHead(teamID, param, value) {
+    const stat = this._head[teamID];
+    const conf = this._config[param];
+    const method = conf.headMethod;
+    const key = conf.key;
+
+    // если метод 'добавление'
+    if (method === '+') {
+      stat[1][key] += value;
+
+      // если метод 'замена'
+    } else if (method === '=') {
+      stat[1][key] = value;
+    }
+
+    this._lastHead.push(stat);
+  }
+
+  // обновляет статистику head синхронизированную с body
+  updateHeadSync(teamID, param, save) {
+    const stat = this._head[teamID];
+    const bodyStats = this._body[teamID];
+    const conf = this._config[param];
+    const method = conf.headMethod;
+    const key = conf.key;
+    let value;
+
+    // если метод 'количество'
+    if (method === '#') {
+      value = Object.keys(bodyStats).length;
+
+      // иначе если метод 'добавление'
+    } else if (method === '+') {
+      value = 0;
+
+      for (const p in bodyStats) {
+        if (bodyStats.hasOwnProperty(p)) {
+          value += bodyStats[p][2][key];
+        }
+      }
+    }
+
+    stat[1][key] = value;
+
+    if (save === true) {
+      this._lastHead.push(stat);
+    }
+  }
+
+  // возвращает последние изменения
+  getLast() {
+    let stat = [this._lastBody, this._lastHead];
+
+    this._lastBody = [];
+    this._lastHead = [];
+
+    if (!stat[0].length && !stat[1].length) {
+      stat = 0;
+    }
+
+    return stat;
+  }
+
+  // возвращает полную статистику
+  getFull() {
+    let stat = [];
+
+    stat[0] = [];
+    stat[1] = [];
+
+    for (const teamID in this._body) {
+      if (this._body.hasOwnProperty(teamID)) {
+        const bodyStats = this._body[teamID];
+
+        for (const gameID in bodyStats) {
+          if (bodyStats.hasOwnProperty(gameID)) {
+            stat[0].push(bodyStats[gameID]);
+          }
+        }
+      }
+    }
+
+    for (const teamID in this._head) {
+      if (this._head.hasOwnProperty(teamID)) {
+        stat[1].push(this._head[teamID]);
+      }
+    }
+
+    if (!stat[0].length && !stat[1].length) {
+      stat = 0;
+    }
+
+    return stat;
+  }
+}
+
+export default Stat;
