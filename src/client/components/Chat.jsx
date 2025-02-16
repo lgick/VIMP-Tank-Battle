982fb0c88b8d16ea672768f59cb3acfee75883a7
lgick <lgick@yandex.ru>
2025-02-17 00:51:55 +0300

    html

---
+++ src/client/components/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div id="chat">
+      <div id="chat-box" />
+      <input id="cmd" type="text" maxLength="60" />
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/client/components/Panel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Panel = () => {
+  return (
+    <div id="panel">
+      <h1 id="logo">VIMP</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td id="panel-health" />
+            <td id="panel-bullets" />
+            <td id="panel-time" />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Panel;
