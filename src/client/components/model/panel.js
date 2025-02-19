4e499cfc127305e1f519c79e3f8d49849d533967
lgick <lgick@yandex.ru>
2025-02-19 03:06:10 +0300

    model

--- src/client/components/model/panel.js
+++ src/client/components/model/panel.js
@@ -1,8 +1,11 @@
-define(['Publisher'], function (Publisher) {
-  // Singleton PanelModel
-  var panelModel;
+import Publisher from '../../../server/lib/publisher.js';
 
-  function PanelModel(data) {
+// Singleton PanelModel
+
+let panelModel;
+
+export default class PanelModel {
+  constructor(data) {
     if (panelModel) {
       return panelModel;
     }
@@ -10,24 +13,16 @@ define(['Publisher'], function (Publisher) {
     panelModel = this;
 
     this._window = data.window;
-
     this._panels = data.panels;
-
     this._timer = null;
-
     this.publisher = new Publisher();
   }
 
   // обновляет данные панели пользователя
-  PanelModel.prototype.update = function (panels) {
-    var i = 0
-      , len = panels.length
-      , panel
-      , value;
-
-    for (; i < len; i += 1) {
-      panel = this._panels[i];
-      value = panels[i];
+  update(panels) {
+    for (let i = 0, len = panels.length; i < len; i += 1) {
+      const panel = this._panels[i];
+      const value = panels[i];
 
       // если данные есть (null - данные игнорируются)
       if (value !== null) {
@@ -35,28 +30,22 @@ define(['Publisher'], function (Publisher) {
         if (panel === 'time') {
           this.updateTime(value);
 
-        // иначе другие данные
+          // иначе другие данные
         } else {
-          this.publisher.emit('data', {
-            name: panel,
-            value: value
-          });
+          this.publisher.emit('data', { name: panel, value });
         }
       }
     }
-  };
+  }
 
   // обновляет время
-  PanelModel.prototype.updateTime = function (time) {
-    var min = ~~(time / 60)
-      , sec = time % 60 || 0
-      , getTime;
+  updateTime(time) {
+    let min = ~~(time / 60);
+    let sec = time % 60 || 0;
 
     this._window.clearInterval(this._timer);
 
-    getTime = (function () {
-      var time;
-
+    const getTime = () => {
       if (sec > 0) {
         sec -= 1;
       } else {
@@ -69,22 +58,13 @@ define(['Publisher'], function (Publisher) {
         }
       }
 
-      if (sec < 10) {
-        time = min + ':' + '0' + sec;
-      } else {
-        time = min + ':' + sec;
-      }
+      const time = sec < 10 ? `${min}:0${sec}` : `${min}:${sec}`;
 
-      this.publisher.emit('data', {
-        name: 'time',
-        value: time
-      });
-    }).bind(this);
+      this.publisher.emit('data', { name: 'time', value: time });
+    };
 
     getTime();
 
     this._timer = this._window.setInterval(getTime, 1000);
-  };
-
-  return PanelModel;
-});
+  }
+}
