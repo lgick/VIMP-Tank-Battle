cdc981b255e049d498f40ede41e842c40441eb72
lgick <lgick@yandex.ru>
2025-03-07 01:18:16 +0300

    bullet

--- src/server/game/config/parts/bullets.js
+++ src/server/game/config/parts/bullets.js
@@ -1,23 +1,23 @@
 export default {
   // pistol
   b1: {
-    time: 5,
-    density: 20,
+    time: 10000,
+    density: 200,
     speed: 30,
     power: 1,
-    width: 10,
-    height: 10,
+    width: 5,
+    height: 5,
     constructor: 'bullet',
   },
 
   // gun
   b2: {
-    time: 4,
-    density: 20,
+    time: 4000,
+    density: 300,
     speed: 30,
     power: 3,
-    width: 10,
-    height: 10,
+    width: 13,
+    height: 13,
     constructor: 'bullet',
   },
 
