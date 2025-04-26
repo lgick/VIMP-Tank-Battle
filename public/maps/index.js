b440a36f2189bc860f59170f83ab8408ad1bde5f
lgick <lgick@yandex.ru>
2025-04-26 12:29:10 +0300

    empty map

--- public/maps/index.js
+++ public/maps/index.js
@@ -1,5 +1,6 @@
 import mini from './mini.js';
 import arena_1x1 from './arena_1x1.js';
 import arena from './arena.js';
+import empty from './empty.js';
 
-export default { mini, arena_1x1, arena };
+export default { mini, arena_1x1, arena, empty };
