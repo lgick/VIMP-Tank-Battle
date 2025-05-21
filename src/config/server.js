c6be098f9b6d2cd228993295cf78d6ac9f0b946c
lgick <lgick@yandex.ru>
2025-05-21 11:01:07 +0300

    wsports

--- src/config/server.js
+++ src/config/server.js
@@ -7,5 +7,9 @@ export default {
   port: 3000,
   oneConnection: false,
   maxPlayers: 10,
+  roundTripTime: {
+    pingInterval: 3000, // интервал обновления rtt (ms)
+    alpha: 0.1, // коэффициент сглаживания для алгоритма экспоненциального скользящего среднего (EMA)
+  },
   VIMP,
 };
