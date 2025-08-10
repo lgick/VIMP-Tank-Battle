b0595fff5735423284734b59211196ae33af773f
lgick <lgick@yandex.ru>
2025-08-10 14:54:54 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite';
 import pugPlugin from 'vite-plugin-pug';
+import fs from 'fs';
+import path from 'path';
+import serverConfig from './src/config/server.js';
+
+const keyPath = serverConfig.httpsOptions.key;
+const certPath = serverConfig.httpsOptions.cert;
+
+// проверка существования файлов, чтобы избежать ошибки при запуске
+if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
+  console.error(`
+    [Vite HMR Error] Certificate files not found!
+    Please ensure these files exist:
+    - Key: ${path.resolve(keyPath)}
+    - Cert: ${path.resolve(certPath)}
+    You might need to generate them first.
+  `);
+
+  process.exit(1);
+}
 
 export default defineConfig({
   plugins: [pugPlugin()],
   server: {
+    https: {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    },
     hmr: {
-      middlewareMode: true,
+      protocol: 'wss',
       port: 3001,
     },
   },
