5760468a0a3e2aaae68a518fcefb87a25c20fe4c
lgick <lgick@yandex.ru>
2025-08-11 09:48:52 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,32 +4,33 @@ import fs from 'fs';
 import path from 'path';
 import serverConfig from './src/config/server.js';
 
-const keyPath = serverConfig.httpsOptions.key;
-const certPath = serverConfig.httpsOptions.cert;
+const isDev = process.env.NODE_ENV === 'development';
+let httpsConfig = false;
 
-// проверка существования файлов, чтобы избежать ошибки при запуске
-if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
-  console.error(`
-    [Vite HMR Error] Certificate files not found!
-    Please ensure these files exist:
-    - Key: ${path.resolve(keyPath)}
-    - Cert: ${path.resolve(certPath)}
-    You might need to generate them first.
-  `);
+if (isDev) {
+  const keyPath = serverConfig.httpsOptions.key;
+  const certPath = serverConfig.httpsOptions.cert;
 
-  process.exit(1);
+  if (!fs.existsSync(keyPath) || !fs.existsSync(certPath)) {
+    console.error(`
+      [Vite HMR Error] Certificate files not found!
+      Please ensure these files exist:
+      - Key: ${path.resolve(keyPath)}
+      - Cert: ${path.resolve(certPath)}
+    `);
+    process.exit(1);
+  }
+
+  httpsConfig = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath),
+  };
 }
 
 export default defineConfig({
   plugins: [pugPlugin()],
   server: {
-    https: {
-      key: fs.readFileSync(keyPath),
-      cert: fs.readFileSync(certPath),
-    },
-    hmr: {
-      protocol: 'wss',
-      port: 3001,
-    },
+    https: httpsConfig,
+    hmr: isDev ? { protocol: 'wss', port: 3001 } : false,
   },
 });
