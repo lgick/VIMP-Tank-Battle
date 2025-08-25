fbbd589ce214ae4d639675ab815e4c404bb79310
lgick <lgick@yandex.ru>
2025-08-25 20:03:16 +0300

    tank sound

--- src/config/sounds.js
+++ src/config/sounds.js
@@ -4,6 +4,11 @@ const sounds = {
   roundStart: 'round-start',
   bombHasBeenPlanted: 'bomb-has-been-planted',
   gameOver: 'game-over',
+  tankEngineIdle: 'tank-engine-idle',
+  tankEngineStart: 'tank-engine-start',
+  tankEngineMove: 'tank-engine-move',
+  tankEngineStop: 'tank-engine-stop',
+  tankTreads: 'tank-treads',
 };
 
 export default {
