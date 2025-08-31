ccd2e33dad63c144ca4b094331c68c38dc9151fe
lgick <lgick@yandex.ru>
2025-09-01 01:30:39 +0300

    map

--- public/maps/index.js
+++ public/maps/index.js
@@ -1,13 +1,9 @@
-import mini from './mini.js';
-import arena1x1 from './arena1x1.js';
-import arena from './arena.js';
 import canopy from './canopy.js';
 import garden from './garden.js';
+import mini from './mini.js';
 
 export default {
   mini,
-  arena1x1,
-  arena,
   canopy,
   garden,
 };
